f36c97c05dd1044bd45b9aacaef641e0ed987f03
Nathan Rhoda <nathan.rhoda@hotmail.com>
2017-08-21 18:23:38 +0200

    First tweet from Python

--- Autocomplain/WebProject1/autocomplain.py
+++ Autocomplain/WebProject1/autocomplain.py
@@ -2,14 +2,14 @@ import time
 import tweepy
 
 def main(): 
-    #auth = tweepy.OAuthHandler(key, secret)
-    #auth.set_access_token(token, token_secret)
-    #client = tweepy.API(auth)
-    #client.update_status("#Python Rocks!")
-    while True:
-        print("Hello World")
-        time.sleep(5)
-
+    key =  'DTSG74qb7twxzGdgMA4GAH9bZ' 
+    secret =  'kfpY2bzDZblf84tkgVXja6laSVW6KQVWXDOrh8b2CeOCrAg8kJ'
+    token_secret = 'qMoWh6KOuzpEFjQraIfmBRRNDKeYGpCbmUooiGE1sqWDO'
+    token = '899614983897657344-JUwDjKobLFwWHmZ8sRJEhlgpaMkx5Ag'
+    auth = tweepy.OAuthHandler(key, secret)
+    auth.set_access_token(token, token_secret)
+    client = tweepy.API(auth)
+    client.update_status("#Test Rocks!")
 
 if __name__ == '__main__':
     main()
