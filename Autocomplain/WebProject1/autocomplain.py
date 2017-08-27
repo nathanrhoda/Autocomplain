1580d14a77c71d88185b596162eab71aa12dbe55
Nathan Rhoda <nathan.rhoda@hotmail.com>
2017-08-27 09:48:04 +0200

    Updated and tweet formatted

--- Autocomplain/WebProject1/autocomplain.py
+++ Autocomplain/WebProject1/autocomplain.py
@@ -1,16 +1,27 @@
 import time
+from datetime import datetime
 import tweepy
 from autocomplain import tweepy
 
-def main(): 
-    key =  'DTSG74qb7twxzGdgMA4GAH9bZ' 
-    secret =  'kfpY2bzDZblf84tkgVXja6laSVW6KQVWXDOrh8b2CeOCrAg8kJ'
-    token_secret = 'qMoWh6KOuzpEFjQraIfmBRRNDKeYGpCbmUooiGE1sqWDO'
-    token = '899614983897657344-JUwDjKobLFwWHmZ8sRJEhlgpaMkx5Ag'
-    auth = tweepy.OAuthHandler(key, secret)
-    auth.set_access_token(token, token_secret)
-    client = tweepy.API(auth)
-    client.update_status("#a Rocks!")
+def main():
+	while(True):
+		dateDiff = datetime.now() - datetime(2017, 8, 1) 
+		days = str(int(dateDiff.total_seconds() / 86400))
+		hours = str(int(dateDiff.total_seconds() / 3600))
+		minutes = str(int(dateDiff.total_seconds() / 60))
+		seconds = str(int(dateDiff.total_seconds()))
+
+		tweet = "@UPSHelp @UPS @UPS_News @pimoroni Tracking No. 1Z8A1E460491377435 " + days + " Days " + hours + " Hours " + minutes + " Minutes " + seconds + " Seconds Still Waiting !!!"
+		time.sleep(5)
+		
+		key =  'DTSG74qb7twxzGdgMA4GAH9bZ' 
+		secret =  'kfpY2bzDZblf84tkgVXja6laSVW6KQVWXDOrh8b2CeOCrAg8kJ'
+		token_secret = 'qMoWh6KOuzpEFjQraIfmBRRNDKeYGpCbmUooiGE1sqWDO'
+		token = '899614983897657344-JUwDjKobLFwWHmZ8sRJEhlgpaMkx5Ag'
+		auth = tweepy.OAuthHandler(key, secret)
+		auth.set_access_token(token, token_secret)
+		client = tweepy.API(auth)
+		client.update_status(tweet)
 
 if __name__ == '__main__':
     main()
